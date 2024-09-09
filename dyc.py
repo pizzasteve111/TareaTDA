b9c9c10bd0fc888fd3f7fb0b71a33a03d002b741
pizzasteve111 <130914989+pizzasteve111@users.noreply.github.com>
2024-09-09 17:16:25 -0300

    Add files via upload

---
+++ dyc.py
@@ -0,0 +1,114 @@
+
+def buscar_joya(arreglo):
+    return wrapper_joya(arreglo,0,len(arreglo)-1)
+
+def wrapper_joya(arr,ini, fin):
+    if ini==fin:
+        return arr[ini]
+    medio=(ini+fin)//2
+    izq=arr[:medio]
+    der=arr[medio+1:]
+    if balanza(izq,der)<0:
+        return wrapper_joya(arr,medio+1,fin)
+    elif balanza(izq,der)>0:
+        return wrapper_joya(arr,ini,medio)
+    else:
+        return arr[medio]
+    
+
+def raiz(n):
+    if n>2:
+        return n
+    return wrapper_raiz(n,1,n//2)
+
+def wrapper_raiz(n,mas_bajo,max):
+    if mas_bajo>max:
+        return max
+    medio = (mas_bajo+max)//2
+    cuadrado= medio*medio
+    if cuadrado==n:
+        return medio
+    elif cuadrado<n:
+        return wrapper_raiz(n,medio+1,max)
+    else:
+        return wrapper_raiz(n,mas_bajo,medio)
+    
+
+
+def KaratsubaOffman(x,y):
+    if x<10 or y<10:
+        return x*y
+    
+    n = max(len(str(x)),len(str(y)))
+    medio= n//2
+
+    #x1 e y1 son las mitades superiores
+   
+
+    x1=x//10**medio
+    x0=x%10**medio
+    y1=y//10**medio
+    y0=y%10**medio
+
+    #se puede evitar una multiplicacion si se hace
+    # un z1 que combine dos terminos
+
+    z2=KaratsubaOffman(x1,y1)
+    z0=KaratsubaOffman(x0,y0)
+    z1=KaratsubaOffman(x1+x0,y1+y0)-z2-z0
+
+    return z2 * 10**(2*medio)+z1*10**medio+z0
+
+
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    medio = len(arr)//2
+    izq=merge_sort(arr[:medio])
+    der= merge_sort(arr[medio:])
+    return merge(izq,der)
+
+def merge(izq,der):
+    if not izq:
+        return der
+    if not der:
+        return izq
+    if izq[0]<der[0]:
+        return [izq[0]] + merge(izq[1:], der)
+    else:
+        return [der[0]]+merge(izq,der[1:])
+    
+#ej 9
+
+def mas_de_la_mitad(arr):
+    if len(arr)==0:
+        return False
+    if len(arr)==1:
+        return True
+    
+    mitad=[]
+    #O(n)
+    for i in range(1,len(arr),2):
+        if arr[i]==arr[i-1]:
+            mitad.append(arr[i])
+
+    
+    candidato = mas_de_la_mitad(mitad)
+
+    #si candidato no esta vacio y las apariciones de candidato en el arr
+    #son mayores a la mitad, True
+    if candidato is not None and arr.count(candidato) > len(arr)//2:
+        return True
+
+    #cantidad impar de elementos
+    if len(arr)%2==1:
+        candidato=arr[-1]
+        if candidato is not None and arr.count(candidato) > len(arr)//2:
+            return True
+
+    return False
+
+    
+
+
+
---
+++ greedy.py
@@ -0,0 +1,203 @@
+#Kruskal crea un MST a partir de las aristas de menor peso a mayor
+#devuelve un grafo sin ciclos y con el peso mínimo entre aristas
+#Es greedy pues en cada paso se elige la arista de menor peso que no provoque ciclos
+#es una decision local que eventualmente da una solucion global optima
+
+#Prim itera a partir de un vertice y mediante un heap va intercalando
+#dicho vertice a aquellas aristas de menor peso
+#es greedy pues el grafo se va a ir expandiendo siempre con la arista de menor peso
+
+#4 SCHEDULING
+
+#Hay que poder dar la mayor cantidad de charlas
+#que no se solapen entre si
+
+#Ordenamos las charlas segun horario de finalizacion
+#osea van primero las charlas que terminan mas temprano
+#e ir agregando charlas que no se solapen entre si
+
+def scheduling(lista_horarios):
+    #ordeno la lista, key recibe la funcion de filtro
+    #lambda ordena de menor a mayor y especifico que es en el segundo elemento de la tupla
+
+    lista_horarios.sort(key = lambda x: x[1])
+
+    res=[]
+
+    #el ultimo horario, para evitar solapamiento
+    ultimo=-1
+
+    for charla in lista_horarios:
+        inicio,fin = charla
+
+        if inicio>=ultimo:
+            res.append(charla)
+            ultimo=fin
+    
+    return res
+
+
+#Minimos billetes: devuelvo primero los primeros
+
+def cambio(lista_billetes, precio):
+    lista_billetes.sort() #O(n log n)
+    lista_billetes.reverse() #O(n)
+    res=[]
+    
+    #O(n)
+    for billete in lista_billetes:
+        while precio>= billete:
+            precio-=billete
+            res.append(billete)
+    
+    return res
+
+#es greedy pues siempre elige la solucion localmente optima, es decir, billete de mayor denominacion
+#que no supere el monto total a pagar.
+
+print(cambio([1,3,4],6))
+#No es optimoo debería devolver dos monedas de 3
+
+def inflacion(prods):
+    j=0
+    total=0
+    prods.sort()
+    prods.reverse()
+    for prod in prods:
+        total+= prod **(j+1)
+        j+=1
+    return total
+
+#la solucion es greedy pues localmente va alamcenando los precios mas altos del día cosa de que termin
+#emos pagando lo minimo debido a la inflacion, de forma que primero pagamos lo mas caro y luego pagamos
+#lo potencialmente mas barato
+
+#Es optimo pues si comprasemos de otra manera siempre terminaríamos gastando mas, lo mejor
+#siempre es comprar lo mas caro antes de que la inflacion lo aumente mucho
+
+
+
+def mochila(elementos,W):
+    #valor/peso nos indica el valor por cantidad de peso
+
+    items=[]
+    peso_actual=0
+    res=[]
+    
+    for valor,peso in elementos:
+        items.append((valor/peso,valor,peso))
+    #ordeno esto de menor relacion valor/peso a mayor
+    items.sort(key=lambda x: x[0])
+    items.reverse()
+
+    for _,valor,peso in items:
+        if peso_actual+peso<=W:
+            res.append((valor,peso))
+            peso_actual+=peso
+        else:
+            continue
+    return res
+
+
+#realizo aquellas que terminan antes
+
+def minimizar_latencia(L_deadline, T_tareas):
+    #Finaliza en F_i= inicio_i+duracion_i
+    #latencia es L_i= F_i - Deadline_i si se supera, sino cero
+    items=[]
+    reloj=0
+    res=[]
+    for i in range (len(L_deadline)):
+        #agrupo el deadline de la tarea con el tiempo que le toma hacerse
+        items.append((L_deadline[i],T_tareas[i]))
+    #ordeno de menor deadline a mayor deadline
+    items.sort(key=lambda x: x[0])
+    
+    for deadline,tiempo in items:
+        
+        if reloj+tiempo>deadline:
+            latencia=(reloj+tiempo)-deadline
+            res.append((tiempo,latencia))
+            
+        else:
+            res.append((tiempo,0))
+        
+        
+        reloj+=tiempo
+
+    
+    return res
+
+def medio(arreglo):
+    medio= len(arreglo)//2
+    return arreglo[medio]
+
+print(medio([156,185,194,242,270]))
+
+def bifurcaciones(ciudades):
+    return wrapper_bifu(ciudades.sort(),0,len(ciudades)-1,[])
+
+def wrapper_bifu(ciudades,ini,fin,res):
+    if ini==fin:
+        return res
+    medio=(ini+fin)//2
+    medio_bifu=ciudades[medio]
+    for bifu in ciudades:
+        if bifu<medio_bifu-50:
+            return wrapper_bifu(ciudades,ini,medio,res)
+        elif bifu>medio_bifu+50:
+            return wrapper_bifu(ciudades,medio,fin,res)
+        else:
+            res.append(bifu)
+
+def bifu(ciudades):
+    res=[]
+    #ordeno por kilometro
+    ciudades.sort(key=lambda x:x[1])
+
+    while ciudades:
+        medio=len(ciudades)//2
+        km_medio=ciudades[medio][1]
+        izq=km_medio-50
+        der=km_medio+50
+
+        no_cubierto=[]
+
+        for ciudad,km in ciudades:
+            if km<izq or km>der:
+                no_cubierto.append((ciudad,km))
+            else:
+                res.append((ciudad,km))
+        ciudades=no_cubierto
+    return res
+
+
+def bolsas(capacidad, productos):
+    bolsas=[]
+    productos.sort()
+    productos.reverse()
+
+    for prod in productos:
+        colocado=False
+        for bolsa in bolsas:
+            if sum(bolsa)+prod<=capacidad:
+                bolsa.append(prod)
+                colocado=True
+                break
+        
+        if not colocado:
+            #nueva bolsa
+            bolsas.append([prod])
+    return bolsas
+
+            
+
+
+
+
+
+
+
+
+
+
