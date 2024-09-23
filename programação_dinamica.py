9bd2e5aa7cf6480590348efb80de87a5c6c762c8
pizzasteve111 <juancardoso123456@gmail.com>
2024-09-23 13:51:47 -0300

    incompleto2

--- programação_dinamica.py
+++ programação_dinamica.py
@@ -34,3 +34,44 @@ def fibonacci(n):
     for i in range(2, n+1):
         M_FIB[i] = M_FIB[i-1] + M_FIB[i-2]
     return M_FIB[n] 
+
+
+#2
+# (inicio,fin,valor)
+#el vector p indica la posicion en la que hay charlas que no se cruzan con el 
+#actual, es decir si n=4 tiene p=2, significa que de la charla 2 para abajo
+#puedo dar cualquiera, esto seria la memorizao paa 
+
+def getP(charlas):
+    p=[-1]*len(charlas)
+    
+    for i in range(len(charlas)):
+        for j in range(i-1,-1,-1):
+            #lo que hago es ver las charlas anteriores cuyos fines
+            #son menores al inicio actual
+            #las voy pisando y me quedo con la mas proxima de las validas
+            if charlas[j][1]<=charlas[i][0]:
+                p[i]=j
+                break
+    return p
+
+# def scheduling(charlas):
+#     #ordeno las charlas por fin mas proximo
+#     charlas.sort(key=lambda x: x[1])
+#     tam=len(charlas)
+#     p=getP(charlas)
+#     optimos=getOptimos(tam,charlas,p)
+#     seleccionadas=getSeleccionadas(optimos,charlas,p)
+#     res=[]
+#     for s in seleccionadas:
+#         res.append[s]
+
+#     return res
+
+# def getOptimos(tam,charlas,p):
+#     optimos=[0] * (tam+1)
+
+#     for i in range(1,tam+1):
+#         indice= i-1
+#             valor_incluir = charlas[charla_index][2] + (optimo_scheduling[p[charla_index] + 1] if p[charla_index] != -1 else 0)
+#         valor_agregar=charlas[indice][2]+
