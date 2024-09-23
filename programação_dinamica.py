9db980c5266b9cc4e910d259ed7a4ce83782865a
pizzasteve111 <130914989+pizzasteve111@users.noreply.github.com>
2024-09-23 15:44:56 -0300

    ej2 resuelto

--- programação_dinamica.py
+++ programação_dinamica.py
@@ -39,7 +39,7 @@ def fibonacci(n):
 #2
 # (inicio,fin,valor)
 #el vector p indica la posicion en la que hay charlas que no se cruzan con el 
-#actual, es decir si n=4 tiene p=2, significa que de la charla 2 para abajo
+#actual, es decir si n=4 tiene p=2, significa que del indice 2 para abajo
 #puedo dar cualquiera, esto seria la memorizao paa 
 
 def getP(charlas):
@@ -55,23 +55,82 @@ def getP(charlas):
                 break
     return p
 
-# def scheduling(charlas):
-#     #ordeno las charlas por fin mas proximo
-#     charlas.sort(key=lambda x: x[1])
-#     tam=len(charlas)
-#     p=getP(charlas)
-#     optimos=getOptimos(tam,charlas,p)
-#     seleccionadas=getSeleccionadas(optimos,charlas,p)
-#     res=[]
-#     for s in seleccionadas:
-#         res.append[s]
+def scheduling(charlas):
+    #ordeno las charlas por fin mas proximo
+    charlas.sort(key=lambda x: x[1])
+    tam=len(charlas)
+    p=getP(charlas)
+    #busco el optimo para cada  cant de charlas
+    optimos=getOptimos(tam,charlas,p)
+    #seleccionadas serian aquellas charlas que van a ir a la solucion optima
+    #el arreglo almacena los indices a dichas charlas
+    seleccionadas=getSeleccionadas(optimos,charlas,p)
+    res=[]
+    for s in seleccionadas:
+        res.append(charlas[s])
 
-#     return res
+    return res
 
-# def getOptimos(tam,charlas,p):
-#     optimos=[0] * (tam+1)
+def getOptimos(tam,charlas,p):
+    #este arreglo sirve para guardar el valor acumulado para la n-esima charla
+    optimos=[0] * (tam+1)
 
-#     for i in range(1,tam+1):
-#         indice= i-1
-#             valor_incluir = charlas[charla_index][2] + (optimo_scheduling[p[charla_index] + 1] if p[charla_index] != -1 else 0)
-#         valor_agregar=charlas[indice][2]+
+    for i in range(1,tam+1):
+        indice= i-1
+        #el valor de la charla actual sobre la que estoy iterando
+        ganancia_actual=charlas[indice][2]
+        #obtengo el optimo actual
+        valor_excluido=optimos[indice]
+
+        #si hay una charla que no se solapa con la actual
+        #entonces el valor actualizado va a ser el valor de dicha charla valida
+        #sumada al valor de la charla actual
+        if p[indice]!=-1:
+            # explico optimos[p[indice]+1] :
+            #p[i] me devuelve el indice de la charla valida para la i-esima charla
+            #entonces quiero el valor optimo para la charla anterior que sea valida
+            #con la actual
+            valor_nuevo=ganancia_actual + optimos[p[indice]+1]
+        else:
+            #en caso de que no hay una charla anterior valida
+            #la charla actual va a tener como acumulado su propio valor
+            valor_nuevo=ganancia_actual
+        
+        
+        
+        #el valor maximo para la charla i va a ser el max entre
+        #su optimo actual o su optimo actualizado 
+        optimos[i]=max(valor_nuevo,valor_excluido)
+
+    return optimos
+
+def getSeleccionadas(optimos,charlas,p):
+    cont=len(charlas)
+    seleccionadas=[]
+
+    while cont>0:
+        indice=cont-1
+        valor_actual=charlas[indice][2]
+        #si existe una charla valida que no se solapa con la actual
+        if p[indice]!=-1:
+            #el nuevo valor sería el de dicha charla + el acumulado
+            #anteriormente
+            valor_actualizau=valor_actual+optimos[p[indice]+1]
+        else:
+            #y si no es asi, solo va a ser el valor de la charla misma
+            valor_actualizau=valor_actual
+        
+
+        valor_excluir=optimos[indice]
+
+        #en caso que tenga que pisar valores
+        #actualizo y cambio el contador para que itere desde la ultima charla modificada
+        if valor_actualizau>valor_excluir:
+            seleccionadas.append(indice)
+            cont=p[indice]+1
+        else:
+            cont-=1
+
+    #hago el reverse porque en mi while estuve iterando de mayor a menor, quiero devolver las charlas en orden inverso
+    seleccionadas.reverse()
+    return seleccionadas
