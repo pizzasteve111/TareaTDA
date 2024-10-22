59035d2b21845b930cbe67fc6acd6de73dbd2b84
pizzasteve111 <juancardoso123456@gmail.com>
2024-10-21 21:50:05 -0300

    lineal casi casi

--- programaçao_lineal.py
+++ programaçao_lineal.py
@@ -93,3 +93,64 @@ def vertex_cover_min(grafo):
 
     return res
 
+#4 Dominating-set minimo = el conjunto mas pequenio posible donde todo v del grafo tiene conexiones con los vertices del set
+#Muy parecido al anterior, solo que no hace falta que todas las aristas tengan por lo menos un vertice, sino que con que este el vertice
+#o alguno de sus adyacentes ya cumple
+
+def dom_set_min(grafo):
+    res=[]
+    j=[]
+    vertices = grafo.obtener_vertices()
+
+    problema=pulp.LpVariable("min_set",pulp.LpMinimize)
+
+    for v in vertices:
+        j[v]=pulp.LpVariable(f"j{v}",cat="Binary")
+
+    for v in  vertices:
+        problema+=j[v] 
+
+    for v in vertices:
+        ws=grafo.adyacentes(v)
+        problema+=j[v] + pulp.Sumatoria(j[w] for w in ws)
+
+    problema.solve()
+
+    res.extend[(v for v in  vertices if pulp.Value(j[v])==1)]
+
+    return res
+
+#5 No idea manito
+
+#6 MST ===> conjunto de aristas que une todos los vertices del grafo y minimiza el peso de sus aristas
+# Se ve compliqueti, todos nuestros vertices tienen que estar conectados en un grafo sin ciclos
+# habria que minimizar la suma de aristas por su peso
+#algo del estilo sum(peso[a]*a) siendo a una arista
+
+def mst_min(grafo):
+    j=[]
+    aristas=grafo.obtener_aristas()
+    vertices=grafo.obtener_vertices()
+
+    problema = pulp.LpVariable("mst_minimize",pulp.LpMinimize)
+
+    #cada arista del grafo es una variable binaria
+    for (v,w) in aristas:
+        j[(v,w)]=pulp.LpVariable(f"j{(v,w)}",cat="Binary")
+
+    #no me acuerdo si existia el metodo obtener peso :P pero de ultima 
+    #la implementation seria la misma solo que hacemos v=>adyacenteW
+    #obtener_peso(v,w)
+    for (v,w) in aristas:
+        problema +=pulp.Sumatoria(grafo.obtener_peso()*j[a])
+
+    #ahora tenemos que asegurar que eun vertice se conecte con una arista
+    for v in vertices:
+        adyacentes=grafo.adyacentes(v)
+        
+        problema+=pulp.LpSum()
+
+    problema.solve()
+
+
+
